95c4fb383859deea987e56376e277e6b863665ce
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2024-03-11 17:17:06 +0100

    feat : loading data

--- src/python/conf/data.py
+++ src/python/conf/data.py
@@ -7,3 +7,16 @@ class Data:
         self.visits = visits
         self.distance_matrix = distance_matrix
         self.time_matrix = time_matrix
+
+
+"""
+    Loading the data from the selected folder
+    :param folder_path: path to the folder
+    :return: Data object
+"""
+def load_data(folder_path):
+    vehicle = read_ini_to_object(folder_path + '/vehicle.ini')
+    visits = read_csv_to_objects(folder_path + '/visits.csv')
+    distance_matrix = load_matrix(folder_path + '/distance.txt')
+    time_matrix = load_matrix(folder_path + '/time.txt')
+    return Data(vehicle, visits, distance_matrix, time_matrix)
